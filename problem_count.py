2d415af160b84d41450b16c8ce68bbf80899f27c
Hide Shinohara <hidenori.shinohara.jp@gmail.com>
2020-02-14 14:13:42 -0500

    Create problem_count.py and replace problem_count.sh with it

---
+++ problem_count.py
@@ -0,0 +1,33 @@
+
+exercise_cnt = []
+problem_cnt = []
+total = 0
+total_chapter_number = 4
+for ch in range(1, total_chapter_number + 1):
+    fobj = open("chapter" + str(ch) + ".tex", "r")
+    excnt = 0
+    prcnt = 0
+    for line in fobj:
+        if "begin{customexer" in line:
+            excnt += 1
+        if "begin{customprob" in line:
+            prcnt += 1
+    exercise_cnt.append(excnt)
+    problem_cnt.append(prcnt)
+    total += excnt + prcnt
+
+print("Total number of problems + exercises: %d" % total)
+print
+
+
+for ch in range(1, total_chapter_number + 1):
+    print("Chapter %d: %2d exercises, %2d problems" % (ch, exercise_cnt[ch - 1], problem_cnt[ch - 1]))
+
+print
+
+for ch in range(1, total_chapter_number + 1):
+    bar = ''
+    for i in range(exercise_cnt[ch - 1] + problem_cnt[ch - 1]):
+        bar += '#'
+    print("Chapter %d: " + bar)
+
--- problem_count.sh
+++
@@ -1,9 +0,0 @@
-for i in 1 2 3 4
-do
-   echo "Chapter $i"
-   echo "  Exercises"
-   tr ' ' '\n' < chapter$i.tex | grep "begin{customexer}" | wc -l
-   echo "  Problems"
-   tr ' ' '\n' < chapter$i.tex | grep "begin{customprob}" | wc -l
-   echo ""
-done
